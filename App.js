c90822e8740f201bab39343d217aef070b66b796
Tanuj Natia <tanujbbsr2k17@gmail.com>
2024-02-17 08:39:00 +0530

    added login and signup screen

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import LandingPage from './components/landingPage';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import BottomNav from "./components/BottomNav";
+import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/Signupscreen";
 
 export default function App() {
-  return (
-    <LandingPage />
-  );
+  // return <SignupScreen />;
+  return <LoginScreen />;
+  // return <RegisterScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
