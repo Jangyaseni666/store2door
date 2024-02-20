54530ea82971065bc99ec38bf8c86c91cd0defb7
Tanuj Natia <tanujbbsr2k17@gmail.com>
2024-02-20 14:18:41 +0530

    Added UserCart page

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import BottomNav from "./components/BottomNav";
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/Signupscreen";
 import Productpage from "./screens/ProductPage";
+import UserCart from "./screens/UserCart";
 
 export default function App() {
   // return <SignupScreen />;
   // return <LoginScreen />;
-  return <Productpage />;
+  // return <Productpage />;
+  return <UserCart />;
 }
 
 const styles = StyleSheet.create({
